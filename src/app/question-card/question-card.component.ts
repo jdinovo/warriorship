a0dd693512443258ab75f2672b27e830391c876e
James DiNovo <james.dinovo@protonmail.com>
2020-03-15 22:50:12 -0400

    Global laoding bar - implemented global loading bar - altered about page

--- src/app/question-card/question-card.component.ts
+++ src/app/question-card/question-card.component.ts
@@ -46,8 +46,7 @@ export class QuestionCardComponent implements OnInit {
     ]);
 
     layoutChanges.subscribe(result => {
-      console.log(result);
-      if(result.breakpoints['(max-width: 600px)']) {
+      if (result.breakpoints['(max-width: 600px)']) {
         this.numOfCols = 1;
         this.rowHeight = '2:1';
       } else if (result.breakpoints['(max-width: 980px)']) {
