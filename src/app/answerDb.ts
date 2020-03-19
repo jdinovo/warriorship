cac999356a3e41c84ecdca1f25afa9a0ec5a07f0
Chris Dias <christopher.dias01@stclairconnect.ca>
2020-03-19 17:47:34 -0400

    Created questions and answers -Added an answerDb -Added the 4 results and content for them -Added the images to be shown with the results

---
+++ src/app/answerDb.ts
@@ -0,0 +1,60 @@
+import {Answer} from './answer-helper';
+
+export let ANSWERS: Answer[] = [
+  {
+    id: 1,
+    title: 'Monk',
+    image: 'src/assets/images/monk.jpg',
+    description: 'You are a monk at heart. You have a spiritual personality' +
+      'with beliefs of your own. When it comes to work, you do what is right without shortcuts.' +
+      'You are self-entitled and live your life in complete happiness.' +
+      'When it comes to difficult moments in your life, you understand that' +
+      'you cannot control everything and that faith will take it\s course.' +
+      'You are the one people come to for advice and they typically follow it.' +
+      'People enjoy what you have to say and your communication skills will come' +
+      'in handy for those future promotions.'
+  },
+  {
+    id: 2,
+    title: 'Viking',
+    image: 'src/assets/images/viking.jpg',
+    description: 'You are a viking at heart. When your mind is set on a goal,' +
+      'you will stop at nothing to achieve it. You have a drive unlike no other.' +
+      'Sometimes you are irrational and will do whatever you please without rhyme or reason.' +
+      'When it comes to heated decisions, you resort to peace over violence, but you\'re not' +
+      'afraid to stand up for yourself when push comes to shove.' +
+      'Just like the vikings, you are artistically inclined and follow the model' +
+      '"work smarter, not harder". You are a tough cookie and not afraid to get your hands' +
+      'dirty in the trenches. You will be successful in life if you keep working hard.'
+  },
+  {
+    id: 3,
+    title: 'Knight',
+    image: 'src/assets/images/knight.jpg',
+    description: 'You are a knight at heart. You can be very simple but sometimes' +
+      'you can be very complex as well. You tend to flip flop on decisions and that is normal.' +
+      'You faced some tough decisions as a child that might have psychologically affected you,' +
+      'and for that reason you are a stronger person today.' +
+      'Just like a knight, you believe in what is right and you fight for it.' +
+      'You are a family person at heart and would do anything for them.' +
+      'Sometimes you may lose your temper and get a little hot headed.' +
+      'Everyday you learn something new and use your new found knowledge to better yourself.' +
+      'You have many life experiences and prefer to live life on the edge when necessary.' +
+      'Your goal is to tell your beautiful stories to your future children and grandchildren.'
+  },
+  {
+    id: 4,
+    title: 'Samurai',
+    image: 'src/assets/images/samurai.jpg',
+    description: 'You are a samurai at heart. When it comes to group projects, you' +
+      'are a team leader. You like to take control of everything happening in your life.' +
+      'Sometimes this could be a disadvantage because some things are out of your control.' +
+      'Your friends and family consider you an honourable and trusting person.' +
+      'You like a challenge and work hard to overcome those tough times in your life and career.' +
+      'Just like a samurai, you have a strong shell on the outside, but' +
+      'sometimes you may bottle your feelings inside and there comes a ' +
+      'point where you may explode on your loved one.' +
+      'You are a risk taker who would rather live life to ' +
+      'the fullest than sitting at home watching TV.'
+  },
+];
