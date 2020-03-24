c061aa556a0678238d600d27357ed0cf21631a05
James DiNovo <james.dinovo@protonmail.com>
2020-03-23 20:46:22 -0400

    Adjusted quiz answer sizing

--- src/app/question-card/question-card.component.ts
+++ src/app/question-card/question-card.component.ts
@@ -51,12 +51,12 @@ export class QuestionCardComponent implements OnInit {
     ]);
 
     layoutChanges.subscribe(result => {
-      if (result.breakpoints['(max-width: 600px)']) {
+      if (result.breakpoints['(max-width: 400px)']) {
         this.numOfCols = 1;
         this.rowHeight = '2:1';
       } else if (result.breakpoints['(max-width: 980px)']) {
         this.numOfCols = 1;
-        this.rowHeight = '5:1';
+        this.rowHeight = '4:1';
       } else {
         this.numOfCols = 2;
         this.rowHeight = '3:1';
